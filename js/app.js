57d70d1724d4058d0e963447d2cd21218052aec3
Jon Veach <veach09@gmail.com>
2019-07-16 17:59:30 -0700

    Changed from object literals to constructor functions

--- js/app.js
+++ js/app.js
@@ -1,98 +1,106 @@
 'use strict';
 
-function getRandomCookie(index) {
-  var min = index.minCustomer;
-  var max = index.maxCustomer;
-  return Math.floor(Math.random() * (max - min + 1) + min);
+var hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
+// var cookiesPerHour = [];
+
+var tablebody = document.getElementById('table');
+var allShops = [];
+
+function Shops(shopName, minCustomersEachHour, maxCustomersEachHour, averageCookiesPerCustomer) {
+  var projectedCustomersPerHour = [];
+  var projectedCookiesPerHour = [];
+  var totalCookiesPerDay = 0;
+  this.shopName = shopName;
+  this.minCustomersEachHour = minCustomersEachHour;
+  this.maxCustomersEachHour = maxCustomersEachHour;
+  this.averageCookiesPerCustomer = averageCookiesPerCustomer;
+  this.projectedCustomersPerHour = projectedCustomersPerHour;
+  this.projectedCookiesPerHour = projectedCookiesPerHour;
+  this.totalCookiesPerDay = totalCookiesPerDay;
+
+  for (var i = 0; i < hours.length; i++) { //Generates projected customers per hour
+    var projectedCustomers = randomNumber(minCustomersEachHour, maxCustomersEachHour);
+    projectedCustomersPerHour.push(projectedCustomers);
+  }
+
+  for (var i = 0; i < hours.length; i++) { //Generates projected cookies per hour based off of customers per hour
+    var projectedCookies = generateCookiesPerHour(averageCookiesPerCustomer, projectedCustomersPerHour[i]);
+    projectedCookiesPerHour.push(projectedCookies);
+  }
+
+  for (var i = 0; i < hours.length; i++) { //Sums the cookies per hour into a total for the day
+    var sumProjectedCookies = sum(projectedCookiesPerHour);
+    this.totalCookiesPerDay = sumProjectedCookies;
+  }
+  projectedCookiesPerHour.push(sumProjectedCookies);
+
+  allShops.push(this);
 }
 
-function calculateCookiesPerDay(index) {
-  var totalCookies = []; //Stores the total cookies into an array
-  var ulEl = document.getElementById(index.elementID);
-  for (var i = 0; i < times.length + 1; i++) {
-    var avgCookieMathFloor = Math.floor(Math.round(index.avgCookiesPS));
-    var salesAvgHour = getRandomCookie(index);
-    var bakeCookies = Math.floor(Math.round(salesAvgHour * avgCookieMathFloor));
-    // console.log(`i = ${i} - times.length - ${times.length}-- ${times[i]} avgCookie = ${avgCookieMathFloor} , salesAvgHour = ${salesAvgHour}, cookies baked ${bakeCookies}`);
-    totalCookies.push(bakeCookies);
-    var liEl = document.createElement('li');
-    liEl.textContent = `${times[i]}: ${bakeCookies} cookies`;
-    ulEl.appendChild(liEl);
-  }
-  totalCookies.pop(); //Cheaty process to drop my ghost array.  But it works so, haters gunna hate
-  var sumTotCookies = 0;
-  for (var j = 0; j < totalCookies.length; j++){ //Loop to add our empty totalCookies array
-    sumTotCookies = totalCookies[j] + sumTotCookies;
-    console.log(`Sum of totcookies ${sumTotCookies}`);
-  }
-  liEl.textContent = `Total: ${sumTotCookies} cookies`;
-  ulEl.appendChild(liEl);
+
+function generateCookiesPerHour(customersPerHour, randomNumber) {
+  return Math.ceil(customersPerHour * randomNumber);
 }
 
-var times = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
 
-var shop1 = {
-  name: '1st and Pike',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookiesPS: 6.3,
-  elementID: 'shop1',
-  render: function() {
-    getRandomCookie(this);
-    calculateCookiesPerDay(this);
-  }
-};
-
-
-var shop2 = {
-  name: 'SeaTac Airport',
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookiesPS: 1.2,
-  elementID: 'shop2',
-  render: function() {
-    getRandomCookie(this);
-    calculateCookiesPerDay(this);
-  }
-};
-
-var shop3 = {
-  name: 'Seattle Center',
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookiesPS: 3.7,
-  elementID: 'shop3',
-  render: function() {
-    getRandomCookie(this);
-    calculateCookiesPerDay(this);
-  }
-};
-
-var shop4 = {
-  name: 'Capitol Hill',
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgCookiesPS: 2.3,
-  elementID: 'shop4',
-  render: function() {
-    getRandomCookie(this);
-    calculateCookiesPerDay(this);
-  }
-};
-
-var shop5 = {
-  name: 'Alki',
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgCookiesPS: 4.6,
-  elementID: 'shop5',
-  render: function() {
-    getRandomCookie(this);
-    calculateCookiesPerDay(this);
-  }
-};
-
-var loadShops = [shop1, shop2, shop3, shop4, shop5];
-for (var i = 0; i < loadShops.length; i++){
-  loadShops[i].render();
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+function sum(arr) {
+  var total = 0;
+  for (var i = 0; i < arr.length; i++) {
+    total = arr[i] + total;
+  }
+  return total;
+}
+
+
+function renderShopName() {
+  var trEl = document.createElement('tr');
+  tablebody.appendChild(trEl);
+  var thEl = document.createElement('th');
+
+  hours.unshift(''); //Puts a blank space at the beginning of my hours
+  hours.push('Total'); //Adds Total at the end of my hours
+
+  for (var i = 0; i < hours.length; i++) {
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+
+}
+
+
+function renderShopBody() {
+  var trEl = document.createElement('tr'); //sends new tr
+  var tdEl = document.createElement('td');
+  tablebody.appendChild(trEl);
+
+  for (var i = 0; i < allShops.length; i++) {
+    trEl = document.createElement('tr');
+    trEl.textContent = allShops[i].shopName;
+    tablebody.appendChild(trEl);
+
+    for (var j = 0; j < allShops[i].projectedCookiesPerHour.length; j++) {
+      tdEl = document.createElement('td');
+      tdEl.textContent = allShops[i].projectedCookiesPerHour[j];
+      trEl.appendChild(tdEl);
+    }
+  }
+}
+
+// function renderShopTotal() {
+// }
+
+new Shops('First & Pike', 23, 65, 6.3);
+new Shops('SeaTac', 3, 24, 1.2);
+new Shops('Seattle Center', 11, 38, 3.7);
+new Shops('Cap Hill', 20, 38, 2.3);
+new Shops('Alki', 2, 16, 4.6);
+
+renderShopName();
+renderShopBody();
+// calculateCookiesPerHour();
